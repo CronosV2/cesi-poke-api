78c2c3a4b56259b3245ff1ef5b67b35fe2639a7d
Cronos <sanderusroleplay@gmail.com>
2025-03-05 16:07:48 +0100

    Create basic Pokemon details page template

---
+++ code-poke-api/src/app/pokemon/[id]/page.tsx
@@ -0,0 +1,13 @@
+interface PokemonPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default function PokemonPage({ params }: PokemonPageProps) {
+  return (
+    <div className="container mx-auto p-4">
+      {/* Template pour la page de détail d'un Pokemon */}
+    </div>
+  );
+} 
